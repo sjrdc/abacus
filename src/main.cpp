7677ba5395787db3b5abcb463099efae817cc85f
sjrdc <scrijns@gmail.com>
2025-03-11 17:07:39 +0100

    Add abacuscli executable

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+/*
+	This file is part of abacus
+	Copyright(C) 2025 Sjoerd Crijns
+
+	This program is free software : you can redistribute it and /or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.If not, see < https://www.gnu.org/licenses/>.
+*/
+
+#include "calculator.h"
+
+#include <iostream>
+#include <string>
+
+namespace
+{
+    std::string string_from_args(int argc, char** argv)
+    {
+        std::vector<std::string> strings(argv + 1, argv + argc);
+        std::string concat;
+        std::for_each(strings.cbegin(), strings.cend(), 
+            [&concat](const auto& s){
+                concat += s;
+            });
+        return concat;
+    }
+}
+int main(int argc, char** argv)
+{
+    if (argc <= 1)
+    {
+        std::cerr << "usage: " << argv[0] << " expression " << std::endl;
+        return 1;
+    }
+
+    const auto expression = string_from_args(argc, argv);
+
+    std::cout << expression << " = " << abacus::calculator::evaluate(expression) << std::endl;
+    return 0;
+}
